677495a530d1fe00d2122060f6dcc60afefb1292
Nick Hart <nhartmusic@gmail.com>
2020-07-24 14:08:55 +0200

    data training, although a bit slow

--- layers.js
+++ layers.js
@@ -7,7 +7,7 @@ const hidden = tf.layers.dense({
 })
 
 const output = tf.layers.dense({
-    units: 3,
+    units: 1,
     activation: 'sigmoid'
 })
 
@@ -18,15 +18,50 @@ const sgdOpt = tf.train.sgd(0.1)
 
 model.compile({
     optimizer: sgdOpt,
-    loss: 'meanSquaredError'
+    loss: tf.losses.meanSquaredError
 })
 
-const inputs = tf.tensor2d([
-    [0.25, 0.92],
-    [0.12, 0.3],
-    [0.4, 0.74],
-    [0.1, 0.22]
+const xs = tf.tensor2d([
+    [0, 0],
+    [0.5, 0.5],
+    [1, 1]
 ])
 
-let outputs = model.predict(inputs)
-outputs.print()
+const ys = tf.tensor2d([
+    [1],
+    [0.5],
+    [0]
+])
+
+
+// const config = {
+//     verbose: true,
+//     epochs: 100
+// }
+
+train().then(() => {
+    console.log('training is complete')
+    let outputs = model.predict(xs)
+    outputs.print()
+})
+
+async function train() {
+    for (let i = 0; i < 100; i++) {
+        const config = {
+            shuffle: true,
+            epochs: 10
+        }
+        const response = await model.fit(xs, ys, config)
+        console.log(response.history.loss[0])
+    }
+}
+
+
+
+// const xs = tf.tensor2d([
+//     [0.25, 0.92],
+//     [0.12, 0.3],
+//     [0.4, 0.74],
+//     [0.1, 0.22]
+// ])
+
